8a028567ed5def1eb9fbb2056120c2822416ce62
Alexander Bleitner <alexanderbleitner@gmail.com>
2021-12-04 00:52:38 +0100

    Formating

--- voucher_server.py
+++ voucher_server.py
@@ -319,7 +319,7 @@ class ReqHandler(http.server.BaseHTTPRequestHandler):
 
         voucher_message = ('''Hallo %s %s,</br>
 </br>
-Sie haben einen %s Gutschein für %s %s bestellt. Bitte überweisen Sie den Betrag von %s Euro auf das folgende Konto um den Gutschein zu aktivieren.
+Sie haben einen %s Gutschein für %s %s bestellt. Bitte überweisen Sie den Betrag von %s Euro auf das folgende Konto um den Gutschein zu aktivieren:</br>
 </br>
 Inhaber: %s</br>
 IBAN: %s</br>
