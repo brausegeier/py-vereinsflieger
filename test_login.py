3cd6faf0fb8ccbca82682aad1bf4247ca325c10e
Brausegeier <spam@brausegeier.de>
2022-01-12 17:52:51 +0100

    Updated login procedure pwd_salt extraction

---
+++ test_login.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+################################################################################
+# py-vereinsflieger/test_login.py
+#
+# Copyright Alexander Bleitner, 2022.
+#
+# License: GPL-3.0-or-later
+################################################################################
+
+
+import vf_api
+from credentials      import Credentials as cred
+
+
+#
+# Instantiate API
+#
+api = vf_api.VF_API(debug=6)
+
+#
+# Provide all credentials
+#
+api.set_credentials(cred().vf_user_id, cred().vf_user_pwd)
+
+#
+# Test Login
+#
+
+api.login()
+api.logout()
+
+
